313c3514ddb5b69d38a67ef7906fdd9aa761d884
楊遠徵 <admin@ssss.fun>
2024-10-21 00:36:39 +0800

    Update app.js

--- app.js
+++ app.js
@@ -5,23 +5,18 @@ function getCurrentTimestamp() {
   const now = new Date();
   return now.toISOString().replace(/T/, ' ').replace(/\..+/, ''); // 格式化为YYYY-MM-DD HH:mm:ss
 }
-
 const app = express();
-const port = 3330;
-
+const port = 3030;
 function delay(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
-
 function sanitizeUrl(url) {
   const urlPattern = /^(https?:\/\/[^\s$]+)/i;
   return urlPattern.test(url) ? url : 'https://error-url.ssss.fun'; // 使用默认 URL
 }
-
 app.get('/screenshot', async (req, res) => {
   const { url = 'https://i.ssss.fun', w: width = 1920, h: height = 1080, t: delaySec = 1 } = req.query;
   const sanitizedUrl = sanitizeUrl(url);
-
   let browser;
   try {
     browser = await puppeteer.launch({
@@ -43,10 +38,8 @@ app.get('/screenshot', async (req, res) => {
     const delayMs = parseInt(delaySec, 10) * 1000;
     await delay(delayMs);
     await page.setViewport({ width: parseInt(width, 10), height: parseInt(height, 10) });
-
     // 执行截图
     const screenshotBuffer = await page.screenshot();
-
     // 发送截图作为响应
     res.set('Content-Type', 'image/png'); // 设置响应头以指示内容类型
     res.send(screenshotBuffer); // 发送截图二进制数据
@@ -64,7 +57,6 @@ app.get('/screenshot', async (req, res) => {
     }
   }
 });
-
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
